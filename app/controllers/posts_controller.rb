9c12ecdfb2a1d4fcdbbb2087487347c9ae8dfa77
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-06-01 01:10:10 +0900

    TODOの基本機能の実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,36 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 
   def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get '/' => "home#top"
   get 'home/about'
   get 'posts/index' => "posts#index"
-  get 'posts/edit'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/destroy'
-  get 'posts/show'
+  get 'posts/:id/edit'=> "posts#edit"
+  get 'posts/new'=> "posts#new"
+  post 'posts/create' => "posts#create"
+  post 'posts/:id/destroy' => "posts#destroy"
+  post 'posts/:id/update' => "posts#update"
+  get 'posts/:id' => "posts#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
